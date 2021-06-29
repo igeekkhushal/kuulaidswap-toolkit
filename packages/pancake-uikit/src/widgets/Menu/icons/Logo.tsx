f8317e17ca4299c56cfcde7ab33b61088ea67062
Alex <king.zan@hotmail.com>
2021-06-29 11:18:45 -0500

    basic changes logo etc

--- packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
+++ packages/pancake-uikit/src/widgets/Menu/icons/Logo.tsx
@@ -10,6 +10,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <Svg viewBox="0 0 160 26" {...props}>
+      <text y="20" className="logoText">PixelSwap</text>
       <image width="205" height="26" href={isDark ? '/images/pixel/LogoDark.gif' : '/images/pixel/LogoWhite.gif'}/>
     </Svg>
   );
